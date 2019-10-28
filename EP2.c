5082d31ddeabda92aeec063cbe33b45b16456c15
Caio Souza <caio.costa@thunderatz.org>
2019-10-28 19:40:35 -0300

    Adiciona a funcionalidade que reconhece as tags

--- EP2.c
+++ EP2.c
@@ -4,6 +4,15 @@
 
 #define TAMANHOMAXIMO 100
 
+#define TRUE 1
+#define FALSE 0
+
+#define TAGABERTURA 1
+#define TAGFECHAMENTO 0
+
+#define TAG 1
+#define PARAMETRO 0
+
 typedef struct _RegPilha {
     char dado[TAMANHOMAXIMO];
     struct _RegPilha *prox;
@@ -62,25 +71,109 @@ void LiberaPilha(Pilha p) {
     free(p);
 }
 
+int TipoDeTag(char caractere) {
+    if (caractere == '/') {
+        return TAGFECHAMENTO;
+    } else {
+        return TAGABERTURA;
+    }
+}
 
+void XMLParaPilha(Pilha p, char arquivo[]) {
+    FILE *fp;
+    long int sizeoffile;
+
+    int abrechaves = FALSE;
+    int natag = FALSE;
+    int tipodetag = TAGABERTURA;
+    int tipodeleitura = TAG;
+
+    fp = fopen(arquivo, "r");
+    if (fp == NULL) {
+        printf("Erro de Leitura\n");
+        exit(-1);
+    }
+
+    fseek(fp, 0, SEEK_END);
+    sizeoffile = ftell(fp);
+    fseek(fp, 0, SEEK_SET);
+
+    char teste[TAMANHOMAXIMO];
+    int posicao = 0;
+    for(int i=0; i<sizeoffile; i++) {
+        char caractere;
+        fscanf(fp, "%c", &caractere);
+
+        if (caractere == '<' && abrechaves == FALSE) {
+            abrechaves = TRUE;
+            //printf("abriu chaves\n");
+            continue;
+        }
+        
+        if (abrechaves == TRUE) {
+            //printf("calculou tipo de tag\n");
+            tipodetag = TipoDeTag(caractere);
+            //printf("Tipo de tag\n", tipodetag);
+            abrechaves = FALSE;
+
+            if (tipodetag == TAGFECHAMENTO) {
+                natag = TRUE;
+                tipodeleitura = TAG;
+                continue;
+            }
+
+            natag = TRUE;
+            tipodeleitura = TAG;
+        }
+
+        if (natag == TRUE && tipodeleitura == TAG) {
+            if (caractere == ' ') {
+                tipodeleitura = PARAMETRO;
+            } else if (caractere == '>') {
+                natag = FALSE;
+                teste[posicao] = '\0';
+                printf("%s %d\n", teste, tipodetag);
+                //AdicionaNaPilha(p, teste);
+                teste[0] = '\0';
+                posicao = 0;
+            } else {
+                teste[posicao] = caractere;
+                posicao++;
+            }
+            continue;
+        }
+
+        if (natag == TRUE && tipodeleitura == PARAMETRO) {
+            if (caractere == '>') {
+                natag = FALSE;
+                teste[posicao] = '\0';
+                printf("%s %d\n", teste, tipodetag);
+                //AdicionaNaPilha(p, teste);
+                teste[0] = '\0';
+                posicao = 0;
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
 
 int main() {
-    Pilha pilha = CriaPilha();
-    AdicionaNaPilha(pilha, "oi");
-    AdicionaNaPilha(pilha, "como");
-    AdicionaNaPilha(pilha, "vai");
+    Pilha pilha;
+    char arquivo[TAMANHOMAXIMO];
 
-    PrintaPilha(pilha);
-
-    printf("\n");
-    printf("%s\n", RemoverDaPilha(pilha));
-    printf("\n");
-
-    PrintaPilha(pilha);
-    printf("\nPilha deletada \n");
-    LiberaPilha(pilha);
-    PrintaPilha(pilha);
-
+    scanf("%s", arquivo);
+    XMLParaPilha(pilha, arquivo);
+    //PrintaPilha(pilha);
 
     return 0;
 }
