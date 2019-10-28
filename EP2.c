7e8818da6fe4d631093c8d3ca4cc17b4b8721698
Caio Souza <caio.costa@thunderatz.org>
2019-10-28 00:37:23 -0300

    Implementa toda a base para funcionamento da pilha

--- EP2.c
+++ EP2.c
@@ -16,7 +16,9 @@ RegPilha* AlocaRegPilha() {
     if (p == NULL) {
         exit(-1);
     }
+
     strcpy(p->dado, "Início da Pilha");
+
     return p;
 }
 
@@ -28,15 +30,19 @@ Pilha CriaPilha() {
 
 void AdicionaNaPilha(Pilha p, char entrada[]) {
     RegPilha *q = AlocaRegPilha();
+
     strcpy(q->dado, entrada);
     q->prox = p->prox;
     p->prox = q;
 }
 
-const char* RemoverDaPilha(Pilha p) {
+char* RemoverDaPilha(Pilha p) {
     RegPilha *q = p->prox;
     p->prox = q->prox;
-    char *saida = q->dado;
+
+    static char saida[TAMANHOMAXIMO];
+    strcpy(saida, q->dado);
+
     free(q);
     return saida;
 }
@@ -49,15 +55,32 @@ void PrintaPilha(Pilha p) {
     PrintaPilha(p->prox);
 }
 
+void LiberaPilha(Pilha p) {
+    if (p->prox != NULL) {
+        LiberaPilha(p->prox);
+    }
+    free(p);
+}
+
+
+
 int main() {
     Pilha pilha = CriaPilha();
     AdicionaNaPilha(pilha, "oi");
     AdicionaNaPilha(pilha, "como");
     AdicionaNaPilha(pilha, "vai");
+
     PrintaPilha(pilha);
+
     printf("\n");
     printf("%s\n", RemoverDaPilha(pilha));
     printf("\n");
+
     PrintaPilha(pilha);
+    printf("\nPilha deletada \n");
+    LiberaPilha(pilha);
+    PrintaPilha(pilha);
+
+
     return 0;
 }
